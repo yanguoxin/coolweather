39f3e049092b9bfaae18ac15e5b1b1bc29634c21
quxl <quxl@snbway.com>
2016-10-26 18:34:32 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/www/guoxin/com/weather/model/City.java
@@ -0,0 +1,45 @@
+package www.guoxin.com.weather.model;
+
+/**
+ * Created by Administrator on 2016/10/26 0026.
+ * City的实体类，方便对City的操作
+ */
+
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/www/guoxin/com/weather/model/Province.java
@@ -0,0 +1,36 @@
+package www.guoxin.com.weather.model;
+
+/**
+ * Created by Administrator on 2016/10/26 0026.
+ * Province的实体类，方便对Province的操作
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
